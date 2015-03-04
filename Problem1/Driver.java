06394abe941dfc696d314bc02eacd1a90a04b1fa
Brian Newsom <brian.newsom@colorado.edu>
2015-03-03 23:19:45 -0700

    Add Problem 1 back in (sorry) and 2

---
+++ Problem1/Driver.java
@@ -0,0 +1,31 @@
+public class Driver {
+
+    public static void main(String[] args) {
+        SubmissionQueue sq1 = SubmissionQueue.getSubmissionQueue();
+
+        Submission[] submissionArray;
+        submissionArray = new Submission[5];
+        for(int i = 0 ; i < 5 ; i++){
+            submissionArray[i] = new Submission();
+        }
+
+        for(Submission submission : submissionArray){
+            SubmissionQueue.add(submission);
+        }
+
+        // Should equal first submission
+        SubmissionQueue.process();
+
+        // Get new submission queue, should return the same instance
+        SubmissionQueue sq2 = SubmissionQueue.getSubmissionQueue();
+
+        // Should be second submission
+        SubmissionQueue.process();
+
+        // Should be third submission
+        sq1.process();
+        SubmissionQueue.process();
+        sq2.process();
+        SubmissionQueue.process();
+    }
+}
---
+++ Problem1/SubmissionQueue.java
@@ -0,0 +1,38 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SubmissionQueue {
+    // Actual FIFO queue to store submissions
+    private static Queue<Submission> myQueue;
+    private static SubmissionQueue mySubmissionQueue;
+
+    //Empty Constructor
+    private SubmissionQueue() {
+      myQueue = new LinkedList<Submission>();
+    }
+
+
+    public static SubmissionQueue getSubmissionQueue() {
+        // If we really want to be correct I think we have to do some java magic here to allow multithreading.
+        if (mySubmissionQueue == null){
+            mySubmissionQueue = new SubmissionQueue();
+        }
+        return mySubmissionQueue;
+    }
+
+    public static void add(Submission s){
+        getSubmissionQueue().myQueue.add(s);
+        System.out.println("Added submission to queue: " + s);
+    }
+
+    public static Submission process(){
+        // Get next element
+        Submission s = getSubmissionQueue().myQueue.poll();
+        if(s == null){
+            System.out.println("Queue empty, nothing removed.");
+        } else {
+            System.out.println("Removed from queue: " + s);
+        }
+        return s;
+    }
+}
